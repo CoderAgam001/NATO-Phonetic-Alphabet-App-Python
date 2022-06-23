ec7bff34f261091507759ef12cc1699c881d878e
Agamdeep Singh <agamdeep579@gmail.com>
2022-06-24 00:00:58 +0530

    Updated the code

--- main.py
+++ main.py
@@ -12,11 +12,10 @@ data = pd.read_csv('data.csv')
 data_dictonary = {row.Letter:row.Code for (index, row) in data.iterrows()}
 
 # Getting the input from the user
-name = input("Enter Your Name: ").upper()
-name_string = name.replace(" ", "")
-name_list = list(name_string)
-selected_words = [data_dictonary[letter] for letter in name_list]
+query = input("Enter The Text: ").lower().replace(" ", "")
+quaery_list = list(query)
+selected_words = [data_dictonary[letter] for letter in quaery_list]
 
 # Printing the output
 seprator = ", "
-print(f"{name} can be spelled as: {seprator.join(selected_words)}")
+print(f"You can spell it as: {seprator.join(selected_words)}")
